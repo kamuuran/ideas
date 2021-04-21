f36ce705cde740a86ee29f478e6b45f5cb2bd8bc
Kamran Maharramov <kamuuran@gmail.com>
2021-04-21 16:35:59 +0400

    en

--- src/localization/en.js
+++ src/localization/en.js
@@ -7,7 +7,7 @@ const en = {
     'copyright': `All rights reserved © 2020-${getYear()}`,
     'makeYourIdeasAlive': 'Make your ideas <span style="font-style: italic; font-weight: 800">alive</span>',
     'communityForTalentedEntrepreneurs': 'Community for talented entrepreneurs',
-    'weOfferOurSkillsAndExperiences': 'We, <span>Ideas.Foundation</span>, offer our own skills and experience to implement your ideas',
+    'weOfferOurSkillsAndExperiences': 'We, <span style="font-style: italic; font-weight: 800">Ideas.Foundation</span>, offer our own skills and experience to implement your ideas',
     'joinedUs': 'Joined us:',
     'IWantToJoin': 'I want to join',
     'allRightsReserved': 'All rights reserved',
