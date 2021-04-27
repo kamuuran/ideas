9df04b5377e532b103b3eba0268af35a8ebdb41f
alish <NacafzadaA@atb.local>
2021-04-27 13:54:39 +0400

    google analytics added

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import LottieAnimation from 'lottie-web-vue'
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-12345678-1',
+  router
+})
 
 new Vue({
   router,
